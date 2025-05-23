9a9f39987905c498726b5dc42ab7985dfecfb056
KillXpert <dharmayu.jadwani@gmail.com>
2025-05-23 19:11:44 +0530

    Resolved the issues mentioned in issues.txt and added multi file support

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,51 @@
 use std::env;
 use std::fs;
 
-fn linecount(filename: &str) -> u32 {
-    let mut lc = 0;
-    for c in filename.chars() {
-        if c == '\n' {
-            lc += 1;
-        }
-    }
-    return lc;
-}
-
 fn main() {
     let arguments: Vec<String> = env::args().skip(1).collect();
 
-    let (modifiers, filename): (Vec<_>, Vec<_>) = arguments
+    let (modifiers, filenames): (Vec<_>, Vec<_>) = arguments
         .into_iter()
         .partition(|x| x.chars().nth(0) == Some('-'));
+
     println!("{:?}", modifiers);
-    println!("{:?}", filename);
+    println!("{:?}", filenames);
     println!("\n\n\n");
 
-    let contents = fs::read_to_string(filename[0].clone()).expect("wrong path");
-    let lc = linecount(&contents);
+    let mut print_ends: bool = false;
+    let mut print_line_nums: bool = false;
+    let merged_modifiers = modifiers.iter().map(|s| s.chars()).flatten();
 
-    if modifiers.len() == 0 {
-        print!("{}", contents);
+    for modi in merged_modifiers {
+        match modi {
+            'e' => print_ends = true,
+            'n' => print_line_nums = true,
+            '-' => continue,
+            _ => {
+                println!("Invalid Arguments");
+                return;
+            }
+        }
     }
 
-    for modi in modifiers {
-        match modi.as_ref() {
-            "-n" => {
-                let mut i: u32 = 1;
-                if contents.len() > 0 {
-                    print!("1  ")
-                }
-                for character in contents.chars() {
-                    print!("{}", character);
-                    if character == '\n' && i < lc {
-                        i += 1;
-                        print!("{}  ", i);
-                    }
-                }
+    let mut new_line: bool = true;
+    let mut i = 0;
+
+    for filename in filenames {
+        let contents = fs::read_to_string(filename.clone()).expect("wrong path");
+        for character in contents.chars() {
+            if new_line && print_line_nums {
+                i += 1;
+                print!("{}  ", i);
+                new_line = false;
             }
-            "-e" => {
-                for character in contents.chars() {
-                    if character == '\n' {
-                        print!("$");
-                    }
-                    print!("{}", character);
+            if character == '\n' {
+                if print_ends {
+                    print!("$");
                 }
+                new_line = true;
             }
-            "-ne" => {
-                let mut condition: bool = true;
-                let mut i = 0;
-                for character in contents.chars() {
-                    if condition {
-                        i += 1;
-                        print!("{}  ", i);
-                        condition = false;
-                    }
-                    if character == '\n' {
-                        print!("$");
-                        condition = true;
-                    }
-                    print!("{}", character);
-                }
-            }
-            "-en" => {
-                let mut condition: bool = true;
-                let mut i = 0;
-                for character in contents.chars() {
-                    if condition {
-                        i += 1;
-                        print!("{}  ", i);
-                        condition = false;
-                    }
-                    if character == '\n' {
-                        print!("$");
-                        condition = true;
-                    }
-                    print!("{}", character);
-                }
-            }
-            &_ => println!("invalid arguments"),
+            print!("{}", character);
         }
     }
 }
